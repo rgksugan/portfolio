dd2c7488094c8f5274d4d29e158d62cb316adea7
Sugan Krishnan <rgk.sugan@gmail.com>
2015-03-11 16:59:31 +0530

    add crude death rate list

--- src/app/viz/viz.controller.js
+++ src/app/viz/viz.controller.js
@@ -2,26 +2,28 @@
 
 angular.module('portfolio').controller('VizCtrl', function ($scope, $http) {
 
-  $scope.status = {
-    isopen: false
-  };
-
-  $scope.selectState = function (state, $event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    $scope.status.isopen = !$scope.status.isopen;
-    $scope.selectedState = state;
-  };
-
-  $scope.$watch('selectedState', function () {
-    var series = _.find($scope.series, function (record) {
-      return record.name === $scope.selectedState;
+  var roadAccidentsHandler = function (response) {
+    var years = _.map(response.data.fields.slice(2), function (field) {
+      return parseInt(field.label);
+    });
+    var series = _.map(response.data.data, function (record) {
+      var tmp = {};
+      tmp.name = record[1];
+      tmp.data = _.map(record.slice(2), function (value) {
+        value = value.replace(/[,]/g, '');
+        value = parseFloat(value);
+        return isNaN(parseFloat(value)) ? null : value;
+      });
+      return tmp;
+    });
+    series = _.find(series, function (record) {
+      return record.name === $scope.stateFilter;
     });
     if (series) {
-      $('#total-number-of-road-accidents-in-india').highcharts({
+      $('#chartContainer').highcharts({
         title: 'Total Number of Road Accidents in India',
         xAxis: {
-          categories: $scope.years,
+          categories: years,
           title: 'Year'
         },
         yAxis: {
@@ -30,22 +32,72 @@ angular.module('portfolio').controller('VizCtrl', function ($scope, $http) {
         series: [series]
       });
     }
-  });
+  };
 
-  $http.get('assets/data/total-number-of-road-accidents-in-india.json').then(function (response) {
-    $scope.years = _.map(response.data.fields.slice(2), function (field) {
+  var crudeDeathRateHandler = function (response) {
+    var years = _.map(response.data.fields.slice(2), function (field) {
       return parseInt(field.label);
     });
-    $scope.series = _.map(response.data.data, function (record) {
+    var series = _.map(response.data.data, function (record) {
       var tmp = {};
       tmp.name = record[1];
       tmp.data = _.map(record.slice(2), function (value) {
         value = value.replace(/[,]/g, '');
-        return parseFloat(value);
+        value = parseFloat(value);
+        return isNaN(parseFloat(value)) ? null : value;
       });
       return tmp;
     });
-    $scope.states = _.pluck($scope.series, 'name');
-    $scope.selectedState = $scope.states[0];
+    series = _.find(series, function (record) {
+      return record.name === $scope.stateFilter;
+    });
+    if (series) {
+      $('#chartContainer').highcharts({
+        title: 'Crude Death Rate in India - 2011',
+        xAxis: {
+          categories: years,
+          title: 'Year'
+        },
+        yAxis: {
+          title: 'Crude Death Rate'
+        },
+        series: [series]
+      });
+    }
+  };
+
+  $scope.charts = [{
+    name: 'Road accidents',
+    url: 'assets/data/total-number-of-road-accidents-in-india.json',
+    handler: roadAccidentsHandler
+  }, {
+    name: 'Crude death rate',
+    url: 'assets/data/crude-death-rate-2011.json',
+    handler: crudeDeathRateHandler
+  }];
+
+  $scope.status = {
+    isopen: false
+  };
+
+  $scope.states = ["Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jammu & Kashmir", "Jharkhand", "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Tripura", "Uttarakhand", "Uttar Pradesh", "West Bengal", "Andaman & Nicobar Islands", "Chandigarh", "Dadra & Nagar Haveli", "Daman & Diu", "Delhi", "Lakshadweep", "Puducherry"];
+
+  $scope.stateFilter = $scope.states[0];
+  $scope.currentChart = $scope.charts[0];
+
+  $scope.selectState = function (state, $event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+    $scope.stateFilter = state;
+  };
+
+  $scope.populateMap = function (chart) {
+    $scope.currentChart = chart;
+    $http.get(chart.url).then(chart.handler);
+  };
+
+  $scope.$watch('stateFilter', function (newValue) {
+    $scope.populateMap($scope.currentChart);
   });
 });
